1ea2ea5cddf107bc4e9c047e9aeddf34b9b0c41e
seheon.oh <seheon.oh@gmail.com>
2024-04-11 22:16:45 +0900

    v1.1

--- src/baygaud.py
+++ src/baygaud.py
@@ -3,6 +3,8 @@
 
 #|-----------------------------------------|
 #| baygaud.py
+#| v1.1
+#| Apr 11 2024
 #|-----------------------------------------|
 #| by Se-Heon Oh
 #| Dept. of Physics and Astronomy
@@ -10,17 +12,11 @@
 #|-----------------------------------------|
 
 
-#|-----------------------------------------|
-# Python 3 compatability
 from __future__ import division, print_function
 
-#|-----------------------------------------|
-# system functions
 import time, sys, os
 from datetime import datetime
 
-#|-----------------------------------------|
-# python packages
 import numpy as np
 from numpy import array
 import psutil
@@ -28,42 +24,23 @@ from multiprocessing import cpu_count
 
 import fitsio
 
-#|-----------------------------------------|
-# import ray
 import ray
 
-#|-----------------------------------------|
-# load baygaudpy modules
-#|-----------------------------------------|
-# _params.py
 from _baygaud_params import default_params, read_configfile
 global _x
 
-#|-----------------------------------------|
-# _dynesty_sampler.py
 from _dynesty_sampler import run_dynesty_sampler_uniform_priors
 from _dynesty_sampler import baygaud_nested_sampling
 from _dynesty_sampler import derive_rms_npoints
 
-#|-----------------------------------------|
-# _fits_io.py
 from _fits_io import read_datacube, moment_analysis
 
-#|-----------------------------------------|
-# import make_dirs
 from _dirs_files import make_dirs
 
-#|-----------------------------------------|
-#|-----------------------------------------|
-# _combine_segs.py
-#import _combine_segs 
 
 
-#  _____________________________________________________________________________  #
-# [_____________________________________________________________________________] #
 def main():
 
-    # read the input datacube
     start = datetime.now()
 
 
@@ -106,42 +83,25 @@ def main():
     gfit_results = np.zeros(((_je-_js), max_ngauss, 2*(2+3*max_ngauss)+7), dtype=np.float32)
 
 
-    #ray.init(num_cpus=3, num_gpus=3)
-    #ray.init(num_cpus=1, ignore_reinit_error=True, object_store_memory=2*10**9)
     required_num_cpus = _ie - _is
-    #ray.init(num_cpus = _params['num_cpus'], dashboard_port=8265, logging_level='DEBUG')
-    #num_cpus = psutil.cpu_count(logical=False)
-    #ray.init(num_cpus=num_cpus)
     ray.init(num_cpus = _params['num_cpus_ray'])
 
 
-    #------------------------------
-    # load the input datacube
     _inputDataCube, _x = read_datacube(_params) # --> _inputDataCub=
 
 
-    # load cube_mask if provided
     if _params['_cube_mask'] == 'Y':
         _cube_mask_2d = fitsio.read(_params['wdir'] + '/' + _params['_cube_mask_2d'])
     elif _params['_cube_mask'] == 'N':
-        # put a dummy array filled with 1
         _cube_mask_2d = np.full((_params['naxis2'], _params['naxis1']), fill_value=1, dtype=np.float32)
 
-    #------------------------------
-    # make the segs output directory
     make_dirs("%s/%s" % (_params['wdir'], _params['_segdir']))
 
-    #------------------------------
-    # derive a rms_med using npoints lines via sgfit --> _params['_rms_med']
     derive_rms_npoints(_inputDataCube, _cube_mask_2d, _x, _params, 1) 
 
-    #------------------------------
-    # derive peak s/n map + integrated s/n map
     _peak_sn_map, _sn_int_map = moment_analysis(_params) #
-    #print(_peak_sn_map)
 
 
-    # ray.put : speed up
     _inputDataCube_id = ray.put(_inputDataCube)
     _peak_sn_map_id = ray.put(_peak_sn_map)
     _sn_int_map_id = ray.put(_sn_int_map)
@@ -153,11 +113,9 @@ def main():
     _js_id = ray.put(_js)
     _je_id = ray.put(_je)
 
-    # nparams: 3*ngauss(x, std, p) + bg + sigma
     _nparams = 3*max_ngauss + 2
 
 
-    #results_ids = [run_dynesty_sampler_uniform_priors.remote(_x_id, _inputDataCube_id, _is_id, _ie_id, i, _js, _je, _max_ngauss_id, _vel_min_id, _vel_max_id) for i in range(_is, _ie)]
 
     results_ids = [baygaud_nested_sampling.remote(_inputDataCube_id, _x_id, \
                                                             _peak_sn_map_id, _sn_int_map_id, \
@@ -168,7 +126,6 @@ def main():
         time.sleep(0.1)
         done_ids, results_ids = ray.wait(results_ids)
         if done_ids:
-            # _xs, _xe, _ys, _ye : variables inside the loop
             _xs = int(ray.get(done_ids)[0][0][max_ngauss-1][2*_nparams+max_ngauss+1])
             _xe = int(ray.get(done_ids)[0][0][max_ngauss-1][2*_nparams+max_ngauss+2])
             _ys = int(ray.get(done_ids)[0][0][max_ngauss-1][2*_nparams+max_ngauss+3])
@@ -176,23 +133,13 @@ def main():
             _curi = int(ray.get(done_ids)[0][0][max_ngauss-1][2*_nparams+max_ngauss+5])
             _curj = int(ray.get(done_ids)[0][0][max_ngauss-1][2*_nparams+max_ngauss+6])
 
-            #print(_xs, _curi, _ys, _curj)
             _segid = _curi-_is+1 # current_i - i_start
-            #makedir_for_curprocess('%s/_seg%d/output_xs%dxe%dys%dye%di%d'
-            #    % (_segdir_, _segid, xs, _xe, _ys, _ye, _segid))
-            #makedir_for_curprocess('%s/_seg%d' % (_segdir_, _segid))
 
-            #print(ray.get(done_ids))
-            #print(array(ray.get(done_ids)).shape)
 
-            # save the fits reults to a binary file
             np.save('%s/%s/G%02d.x%d.ys%dye%d' % (_params['wdir'], _params['_segdir'], max_ngauss, _curi, _ys, _ye), array(ray.get(done_ids)))
 
-    #results_compile = ray.get(results_ids)
-    #print(results_compile)
     ray.shutdown()
     print("duration =", datetime.now() - start)
-#-- END OF SUB-ROUTINE____________________________________________________________#
 
 if __name__ == '__main__':
     main()
